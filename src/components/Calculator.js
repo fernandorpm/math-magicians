b3a99f12ebea13eec326d33af50be556d4a9e0e5
Fernando Marques <frpm1001@gmail.com>
2021-08-10 12:39:12 -0300

    Add layout of the calculator

---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import './Calculator.css';
+import CalcButton from './CalcButton';
+
+function Calculator() {
+  return (
+    <div className="Calculator">
+      <div className="Calculator-result">0</div>
+
+      <CalcButton character="AC" />
+      <CalcButton character="+/-" />
+      <CalcButton character="%" />
+      <CalcButton character="+" classButton="orangeButton" />
+
+      <CalcButton character="7" />
+      <CalcButton character="8" />
+      <CalcButton character="9" />
+      <CalcButton character="x" classButton="orangeButton" />
+
+      <CalcButton character="4" />
+      <CalcButton character="5" />
+      <CalcButton character="6" />
+      <CalcButton character="-" classButton="orangeButton" />
+
+      <CalcButton character="1" />
+      <CalcButton character="2" />
+      <CalcButton character="3" />
+      <CalcButton character="/" classButton="orangeButton" />
+
+      <CalcButton character="0" />
+      <CalcButton character="." />
+      <CalcButton character="=" classButton="orangeButton" />
+
+    </div>
+  );
+}
+
+export default Calculator;
